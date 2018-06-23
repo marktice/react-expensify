57178b6f3f0a3f82187a8d3f8033390e72eddc2b
Mark Tice <marktice@protonmail.com>
2018-06-23 18:13:33 +1000

    Split router and components into serperate files

--- src/app.js
+++ src/app.js
@@ -1,62 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
+
+import AppRouter from './routers/AppRouter';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
-const ExpenseDashboardPage = () => {
-  return <div>This is from my dashboard component</div>;
-};
-const AddExpensePage = () => {
-  return <div>This is from my add expense component</div>;
-};
-const EditExpensePage = () => {
-  return <div>This is from my edit expense component</div>;
-};
-const HelpPage = () => {
-  return <div>This is from my help page component</div>;
-};
-const NotFoundPage = () => {
-  return (
-    <div>
-      404 - <Link to="/">Go Home</Link>
-    </div>
-  );
-};
-const Header = () => {
-  return (
-    <header>
-      <h1>Expensify</h1>
-      <NavLink exact to="/" activeClassName="is-active">
-        Dashboard
-      </NavLink>
-      <NavLink to="/create" activeClassName="is-active">
-        Create Expense
-      </NavLink>
-      <NavLink to="/edit" activeClassName="is-active">
-        Edit Expense
-      </NavLink>
-      <NavLink to="/help" activeClassName="is-active">
-        Help
-      </NavLink>
-    </header>
-  );
-};
-
-const routes = (
-  <BrowserRouter>
-    <div>
-      <Header />
-      <Switch>
-        <Route exact path="/" component={ExpenseDashboardPage} />
-        <Route path="/create" component={AddExpensePage} />
-        <Route path="/edit" component={EditExpensePage} />
-        <Route path="/help" component={HelpPage} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </div>
-  </BrowserRouter>
-);
-
-ReactDOM.render(routes, document.querySelector('#app'));
+ReactDOM.render(<AppRouter />, document.querySelector('#app'));
---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const EditExpensePage = (props) => {
+  console.log(props);
+  return (
+    <div>This is from my edit expense component of {props.match.params.id}</div>
+  );
+};
+
+export default EditExpensePage;
