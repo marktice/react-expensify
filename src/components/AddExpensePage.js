ab77100aa6ada8264d07b94531c1942b194c5460
Mark Tice <marktice@protonmail.com>
2018-08-01 19:16:55 +1000

    Add redux add expense and expense list

--- src/components/AddExpensePage.js
+++ src/components/AddExpensePage.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import ExpenseForm from './ExpenseForm';
 
 const AddExpensePage = () => {
-  return <div>This is from my add expense component</div>;
+  return (
+    <div>
+      <h1>Add Expense</h1>
+      <ExpenseForm />
+    </div>
+  );
 };
 
 export default AddExpensePage;
