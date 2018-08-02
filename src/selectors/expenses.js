76f30462584360e15cf34214e782a1412590779f
Mark Tice <marktice@protonmail.com>
2018-08-02 19:27:40 +1000

    Add tests for expenses selector

--- src/selectors/expenses.js
+++ src/selectors/expenses.js
@@ -1,7 +1,7 @@
 import moment from 'moment';
 
 // Get visible expenses
-const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
+export default (expenses, { text, sortBy, startDate, endDate }) => {
   return expenses
     .filter((expense) => {
       const createdAtMoment = moment(expense.createdAt);
@@ -21,5 +21,3 @@ const getVisibleExpenses = (expenses, { text, sortBy, startDate, endDate }) => {
       }
     });
 };
-
-export default getVisibleExpenses;
